97f92b62e5ad051bd2caed4ff04808bcc4e0119a
amedello <50532643+amedello@users.noreply.github.com>
2026-06-24 14:47:44 +0200

    feat(iframe-view): add IframeView full-body iframe component

---
+++ src/lib/Main/IframeView.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let url: string;
+</script>
+
+<main>
+	<iframe src={url} title="iframe" />
+</main>
+
+<style>
+	main {
+		grid-area: main;
+		display: grid;
+		overflow: hidden;
+	}
+
+	iframe {
+		width: 100%;
+		height: 100%;
+		border: none;
+		display: block;
+	}
+</style>
